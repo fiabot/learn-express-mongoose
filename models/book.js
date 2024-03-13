11879a20c4a3af955b8dedd09880c05deaa060fb
Fiona Shyne <shynef@union.edu>
2024-03-13 15:48:57 -0400

    updated schema for book and genre

--- models/book.js
+++ models/book.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema(
-  {}
+  {
+    title : {type: String, required: true}, 
+    author: { type: Schema.Types.ObjectId, ref: 'Author', required: true }, 
+    title : {type: String}, 
+    isbn: {type: String, required: true}, 
+    genre: { type: Schema.Types.ObjectId, ref: 'Genre', required: true }
+  }
 );
 
 //Export model
